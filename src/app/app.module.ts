e38a515a313047fac6732d9d548eef0b31b7e093
Javier Sebastian <u20211e429@upc.edu.pe>
2023-09-25 20:20:58 -0500

    fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ToolbarComponent } from './components/toolbar/toolbar.component';
 import {MatToolbarModule} from '@angular/material/toolbar';
-import {SharedModule} from './shared/shared.module';
+import { SharedModule } from 'src/public/shared/shared.module';
 
 @NgModule({
   declarations: [
